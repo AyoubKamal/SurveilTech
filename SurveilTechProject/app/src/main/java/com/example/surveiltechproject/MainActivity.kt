481e667a0f7863648361c701730e6b38639c7baf
KAMAL AYOUB <ayoubkamal2011@gmail.com>
2024-05-13 14:55:29 -0400

    SecondCommit

--- SurveilTechProject/app/src/main/java/com/example/surveiltechproject/MainActivity.kt
+++ SurveilTechProject/app/src/main/java/com/example/surveiltechproject/MainActivity.kt
@@ -5,16 +5,41 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
+import java.net.InetAddress
+import java.net.NetworkInterface
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textViewIPAddress: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        textViewIPAddress = findViewById(R.id.textViewIPAddress)
+        val ipAddress = getDeviceIPAddress()
+        textViewIPAddress.text = "IP Address: $ipAddress"
+    }
+
+
+
+    private fun getDeviceIPAddress(): String {
+        try {
+            val interfaces: Enumeration<NetworkInterface> = NetworkInterface.getNetworkInterfaces()
+            while (interfaces.hasMoreElements()) {
+                val iface: NetworkInterface = interfaces.nextElement()
+                val addresses: Enumeration<InetAddress> = iface.inetAddresses
+                while (addresses.hasMoreElements()) {
+                    val addr: InetAddress = addresses.nextElement()
+                    if (!addr.isLoopbackAddress) {
+                        return addr.hostAddress
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
+        return "IP address not found"
     }
 }
